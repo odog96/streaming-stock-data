151daac117a797f74b20f1e1a475d10934e1f48b
Oliver Zarate <ozarate@cloudera.com>
2023-01-10 21:03:11 +0000

    udpated cdsw-build file w new pckgs

--- cdsw-build.sh
+++ cdsw-build.sh
@@ -1,4 +1,10 @@
 pip3 install sklearn
+pip3 install pandas
+pip3 install yfinance
+pip3 install scikit-learn
+pip3 install seaborn
+pip3 install tensorflow
+
 
 if command -v pip2 >/dev/null 2>&1; then
    pip2 install sklearn
